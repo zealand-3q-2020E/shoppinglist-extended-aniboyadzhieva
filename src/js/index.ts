ac13f392eacb34a4a3d4ee9d688a2fc87f744125
aniboyadzhieva <anix0072@edu.easj.dk>
2020-09-29 13:54:18 +0200

    typeScript unhealthy list

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,19 @@
 //Write your Typescript code here
+
+document.getElementById("AddButton").addEventListener('click', addItem);
+let input = document.getElementById("newItem");
+let unhealthyList = document.getElementById("listUnhealthy"); 
+
+function addItem(e:MouseEvent)
+{
+  console.log("in the addItem function");
+  let inputText = (<HTMLInputElement>input).value;
+
+  console.log("input:" + inputText);
+
+  let newLi = document.createElement('li');
+  let newText = document.createTextNode(inputText)
+  newLi.appendChild(newText)
+  newLi.setAttribute('class', 'unhealthy')
+  unhealthyList.appendChild(newLi)
+}
